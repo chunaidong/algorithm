032c245664b2fd847432191a7ee0f1c20ce969cc
zhuxiaobao <2838138608@qq.com>
2019-03-11 22:40:38 +0800

    add insertionSort2

--- src/com/chunaidong/algorithm/firstday/insertion/BasicInsertSort.java
+++ src/com/chunaidong/algorithm/firstday/insertion/BasicInsertSort.java
@@ -8,6 +8,7 @@ public class BasicInsertSort {
      * @param arrays
      */
     public static void insertSort(int[] arrays){
+        // 3 5 6 1 2
         for(int i = 1 ; i < arrays.length ; i++){
             for (int j = i ; j > 0 ; j--){
                 if(arrays[j] < arrays[j-1]){
@@ -20,6 +21,28 @@ public class BasicInsertSort {
             }
         }
     }
+
+    /**
+     * 插入算法
+     * @param arrays
+     */
+    public static void insertSort2(int[] arrays){
+        // 3 5 6 1 2
+        // i =3;
+        for(int i = 1 ; i < arrays.length ; i++){
+            int tmp = arrays[i];
+            int j;
+            for (j = i ; j > 0 ; j--){
+                if(arrays[j-1] > tmp){
+                   arrays[j] = arrays[j-1];
+                }else {
+                    break;
+                }
+            }
+            arrays[j] = tmp;
+        }
+    }
+
 
 
 
